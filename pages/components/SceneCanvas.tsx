bfcdbd8bf5b38ffc6c653f29834784702ae6277f
Malik Kotb <malikkotb@icloud.com>
2024-02-11 13:22:15 +0100

    model on scroll

--- pages/components/SceneCanvas.tsx
+++ pages/components/SceneCanvas.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useScroll } from "framer-motion";
 import { motion } from "framer-motion-3d";
 import Model from "./Model";
 
@@ -9,35 +8,39 @@ import { Canvas } from "@react-three/fiber";
 import { Stage } from "@react-three/drei";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useScroll } from "framer-motion";
 
 export default function SceneCanvas({ source }: { source: string }) {
+  // framer 3d apporach
   const sceneCanvas = useRef(null);
+  // const { scrollYProgress } = useScroll({
+  //   target: sceneCanvas,
+  //   offset: ["start end", "end start"],
+  // });
 
-  const { scrollYProgress } = useScroll({
-    target: sceneCanvas,
-
-    offset: ["start end", "end start"],
-  });
-
+  // custom approach
   const [scrollY, setScrollY] = useState(0);
-
   useEffect(() => {
     const handleScroll = () => setScrollY(window.pageYOffset);
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
-  // Convert scroll position to rotation (you might need to adjust the divisor to control the speed of rotation)
   const rotationY = scrollY * 0.001;
 
   return (
-    <div className="flex justify-center items-center h-full">
-      <Canvas ref={sceneCanvas}>
+    <div
+      ref={sceneCanvas}
+      className="flex justify-center items-center h-full"
+    >
+      <Canvas>
         <ambientLight intensity={0.5} />
         <directionalLight position={[-2, 5, 2]} intensity={1} />
         <Stage environment={"night"}>
-          <motion.group rotation={[0, rotationY, 0]}>
+          <motion.group
+            // rotation-y={scrollYProgress}
+            rotation={[0, rotationY, 0]}
+          >
             <Model source={source} />
           </motion.group>
         </Stage>
