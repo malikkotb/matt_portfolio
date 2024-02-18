623513f6dfba6093d9b95d9750cc513dbe80d0c6
Malik Kotb <malikkotb@icloud.com>
2024-02-18 23:05:17 +0100

    image animation grid setup

--- pages/yacht/index.tsx
+++ pages/yacht/index.tsx
@@ -1,14 +1,23 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useEffect } from "react";
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import { motion, useMotionValue, useSpring, useScroll, useTransform } from "framer-motion";
 
 import SceneCanvas from "../components/Model_Interactive/SceneCanvas";
 import { IoArrowDownSharp } from "react-icons/io5";
-import { useScroll } from "framer-motion";
+
+const divStyles = [
+  { top: "10vh", left: "10vw", width: "45vw", height: "25vh" },
+  { top: "30vh", left: "40vw", width: "45vw", height: "25vh" },
+  { top: "50vh", left: "5vw", width: "25vw", height: "25vh" },
+  { top: "40vh", left: "40vw", width: "40vw", height: "25vh" },
+  { top: "60vh", left: "60vw", width: "30vw", height: "25vh" },
+  { top: "70vh", left: "15vw", width: "45vw", height: "25vh" },
+  { top: "90vh", left: "50vw", width: "45vw", height: "25vh" },
+  { top: "120vh", left: "10vw", width: "40vw", height: "25vh" },
+];
 
 export default function index() {
-
   // custom Cursor
   const cursorSize = 15;
   const mouse = {
@@ -36,10 +45,16 @@ export default function index() {
     };
   }, []);
 
-  // Images Animation (useScroll)
+  // Images Animation
+  const containerRef = useRef(null)
+  const targetRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({ target: targetRef, offset: ["0 1", "1.33 1"] });
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1])
 
   return (
-    <main className="bg-[#E5E4E0]">
+    <main className="bg-[#E5E4E0] overflow-x-hidden">
       {/* custom cursor */}
       <motion.div
         animate={{ scale: isHovered ? 8 : 1 }}
@@ -52,54 +67,43 @@ export default function index() {
         }}
         className="fixed w-5 h-5 z-50 rounded-full pointer-events-none"
       ></motion.div>
-      {/* 3D Model */}
       <div className="fixed w-screen h-screen z-20">
         <SceneCanvas source={"./yacht.glb"} />
       </div>
       <div className="h-[100vh] text-center">
         <div className="z-30 absolute text-[12vh] text-[#4A4049] font-medium top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3 flex flex-col items-center">
-          <span className="mb-4 ml-[-50vw]" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+          <span
+            className="mb-4 ml-[-50vw]"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
             MINIJAM
           </span>
-          <span className="mb-8 ml-[50vw]" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+          <span
+            className="mb-8 ml-[50vw]"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
             2023
           </span>
-          <span className=" mt-24 ml-[-40vw]" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+          <span
+            className=" mt-24 ml-[-40vw]"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
             NOWLIVE
           </span>
         </div>
         <div className="absolute top-[95%] left-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-2 items-center z-20">
-            {/* TODO: make the scroll text dissappear on scroll */}
-          SCROLL <IoArrowDownSharp />
+          SCROLL <IoArrowDownSharp /> {/* TODO: make text dissapear on scroll */}
         </div>
       </div>
-      <div className="h-[100vh] w-screen">
-        <div className="absolute z-30">
-          <img src="./1.jpg" alt="1" className="w-[45vw] ml-20 mt-48" />
-        </div>
-      </div>
-      <div className="h-[100vh] w-screen">
-        <div className="absolute z-30">
-          <img src="./2.jpg" alt="1" className="w-[25vw] ml-[50vw] mt-24" />
-        </div>
-      </div>
-      <div className="h-[100vh] w-screen">
-        <div className="absolute z-30">
-          <img src="./3.jpg" alt="1" className="w-[20vw] h-[70vh] ml-40 mt-24" />
-        </div>
-        <div className="absolute z-30">
-          <img src="./6.jpg" alt="1" className="w-[25vw] ml-[40vw] mt-12" />
-        </div>
-      </div>
-      <div className="h-[100vh] w-screen">
-        <div className="absolute z-30">
-          <img src="./4.jpg" alt="1" className="w-[45vw] ml-20 mt-24" />
-        </div>
-      </div>
-      <div className="h-[100vh] w-screen">
-        <div className="absolute z-30">
-          <img src="./5.jpg" alt="1" className="w-[45vw] ml-[50vw] mt-48" />
-        </div>
+      <div ref={targetRef} className="h-[320vh] relative">
+        {divStyles.map((style, index) => (
+          <motion.div key={index} className="relative z-30" style={{ scale: scaleProgress, top: style.top, left: style.left, width: style.width, height: style.height }}>
+            <img src={`./${index + 1}.jpg`} alt={`${index + 1}`} className="" />
+          </motion.div>
+        ))}
       </div>
     </main>
   );
